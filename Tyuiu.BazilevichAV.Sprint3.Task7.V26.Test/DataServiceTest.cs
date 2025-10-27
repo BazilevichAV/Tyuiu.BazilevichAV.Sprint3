72bee70dba478629da19799c7dd5a687e5b289d7
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-10-27 14:11:00 +0500

    Task7

---
+++ Tyuiu.BazilevichAV.Sprint3.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BazilevichAV.Sprint3.Task7.V26.Lib;
+namespace Tyuiu.BazilevichAV.Sprint3.Task7.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 20.19;
+            valueWaitArray[1] = 17.21;
+            valueWaitArray[2] = 14.13;
+            valueWaitArray[3] = 11.02;
+            valueWaitArray[4] = 8.06;
+            valueWaitArray[5] = 7.00;
+            valueWaitArray[6] = 0.77;
+            valueWaitArray[7] = -1.55;
+            valueWaitArray[8] = -4.21;
+            valueWaitArray[9] = -7.03;
+            valueWaitArray[10] = -10.00;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint3.Task7.V26/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Tyuiu.BazilevichAV.Sprint3.Task7.V26.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила: Базилевич А.В. | ИБКСб-25-1";
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* Спринт #3                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту *");
+Console.WriteLine("* Задание #7                                              *");
+Console.WriteLine("* Вариант #26                                             *");
+Console.WriteLine("* Выполнила: Базилевич Алёна Владимировна | ИБКСб-25-1    *");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                *");
+Console.WriteLine("* Напишите программу, которая выводит таблицу значений    *");
+Console.WriteLine("* функции на заданном диапазоне [-5;5] с шагом 1.         *");
+Console.WriteLine("* Произвести проверку деления на 0. При делении на 0      *");
+Console.WriteLine("* вернуть значение 0. Округлить до 2 знаков после запятой.*");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+Console.WriteLine("***********************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+double[] valueArray;
+valueArray = new double[len];
+valueArray = ds.GetMassFunction(startValue, stopValue);
+
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+Console.WriteLine("***********************************************************");
+
+Console.WriteLine("+---------+---------+");
+Console.WriteLine("+    Х    +   f(x)  +");
+Console.WriteLine("+---------+---------+");
+
+for (int i = 0; i <= len-1; i++)
+{
+    Console.WriteLine("| {0,5:d}   |  {1,5:f2}  |", startValue, valueArray[i]);
+    startValue++;
+}
+
+Console.WriteLine("+---------+---------+");
+Console.ReadKey();
